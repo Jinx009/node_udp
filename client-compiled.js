4007c50aa5405b4144b80acb8971b6d2aef59442
Jinx <jinxlovejinx@vip.qq.com>
2018-11-27 10:30:02 +0800

    update function

--- client-compiled.js
+++ client-compiled.js
@@ -5,7 +5,9 @@ var message = new Buffer("000118103000000768FFEA00001800034A01FF9D000063000B7E")
 
 var client = dgram.createSocket("udp4");
 
-client.send(message,0,message.length,7777,"106.14.94.245",function(err,bytes){
+client.send(message, 0, message.length, 7777, "106.14.94.245", function (err, bytes) {
     console.log(err);
     client.close();
 });
+
+//# sourceMappingURL=client-compiled.js.map
